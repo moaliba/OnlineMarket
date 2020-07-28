b3ec2d1d7ae48e04cbd8fcdd8459d4e4422d4835
Mohammad Ali Barati <Mohammad.Ali.Barati@Gmail.com>
2020-07-28 23:19:27 +0330

    Add dbcontext to data project and introduce it to web API project.

--- OnlineMarket.API/Startup.cs
+++ OnlineMarket.API/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using OnlineMarket.Data;
 
 namespace OnlineMarket.API
 {
@@ -25,6 +28,8 @@ namespace OnlineMarket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OnlineMarketDBContext>(options=> 
+                    options.UseSqlServer(Configuration.GetConnectionString("OnlineMarketDBConnection")));
             services.AddControllers();
         }
 
---
+++ OnlineMarket.Data/OnlineMarketDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMarket.Data.Configuration;
+using OnlineMarket.Data.Configurations;
+using OnlineMarket.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineMarket.Data
+{
+    public class OnlineMarketDBContext: DbContext
+    {
+        public OnlineMarketDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sell> Sells { get; set; }
+
+        public DbSet<WarehouseEntrance> WarehouseEntrances { get; set; }
+
+        public DbSet<ExpenceDocument> ExpenceDocuments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+            builder.ApplyConfiguration(new SellConfiguration());
+            builder.ApplyConfiguration(new WarehouseEntranceConfiguration());
+            builder.ApplyConfiguration(new ExpenceDocumentConfiguration());
+        }
+
+
+    }
+}
